493fd4a24c0b0fd25184a3d9764517e4e844713f
omrikap <omrikap@gmail.com>
2015-08-09 16:51:34 +0300

    lineToVector implemented

--- LineSeparator.c
+++ LineSeparator.c
@@ -16,15 +16,6 @@
 
 // ------------------------------ functions -----------------------------
 
-
-/**
- * A struct that represents a vector, with maximum 74 coordinates.
- */
-typedef struct vector {
-	int _label;
-	double _coordinates[MAX_VECTOR_LENGTH];
-} vector;
-
 /**
  * @brief The Standard Inner Product function of two vector structs.
  * @param vector1 the first vector.
@@ -39,6 +30,19 @@ double vectorInnerProduct (vector, vector);
  */
 vector vectorAddition(vector, vector);
 
+/**
+ * @brief todo
+ */
+vector scalarMultiplication(vector, double);
+
+/**
+ * @brief The function that calculates the position of the hyper-...
+ * todo complete
+ */
+void updateVector(vector, vector, int);
+
+vector lineToVector(char*, int);
+
 // todo parse file
 int parseFile(FILE*);
 
@@ -52,7 +56,17 @@ vector vectorAddition(vector vector1, vector vector2)
 	return result;
 }
 
-double vectorInnerProduct (vector vector1, vector vector2)
+vector scalarMultiplication(vector theVector, double scalar) // todo test
+{
+	vector result = theVector;
+	for (int i = 0; i < MAX_VECTOR_LENGTH; ++i)
+	{
+		result._coordinates[i] = theVector._coordinates[i] * scalar;
+	}
+	return result;
+}
+
+double vectorInnerProduct(vector vector1, vector vector2)
 {
 	double result = 0;
 	for (int i=0; i<MAX_VECTOR_LENGTH; ++i)
@@ -62,12 +76,49 @@ double vectorInnerProduct (vector vector1, vector vector2)
 	return result;
 }
 
+vector lineToVector(char* line, int dimension)
+{
+	vector x;
+	int index = 0;
+	x._coordinates[index] = atof(strtok(line, COORDINATE_SEPARATOR));
+	printf("%d'th coordinate: %f\n", index, x._coordinates[index]);
+	++index;
+	for (; index < dimension; ++index)
+	{
+		x._coordinates[index] = atof(strtok(NULL, COORDINATE_SEPARATOR));
+		printf("%d'th coordinate: %f\n", index, x._coordinates[index]);
+	}
+	x._label = atoi(strtok(NULL, COORDINATE_SEPARATOR));
+	return x;
+}
+
+void updateVector( vector separator, vector dataVector, int tag) // todo change dataVector name,
+// implement
+{
+
+}
+
+int trainingSession(FILE *pFile, int dimension, int trainingExamples, vector separator)
+{
+	char line[151];
+	puts("here"); // todo remove
+	for (int lineNumber = 0; lineNumber < trainingExamples; ++lineNumber)
+	{
+		fgets(line, 151, pFile);
+		vector dataVector = lineToVector(line, dimension);
+//		updateVector(separator, dataVector, dataVector._label);
+		printf("dataVector._label: %d\n", dataVector._label);
+	}
+//	puts("returning"); // todo remove
+	return 0;
+}
+
 int parseFile(FILE *theFile)
 {
 	int dimension = 0;
 	int trainingExamples = 0;
 	char line[151];
-	for (int i = 0; i < 2; ++i) // todo change logic
+	for (int i = 0; i < 2; ++i) // todo improve logic
 	{
 		fgets(line, 151, theFile);
 		if (i == 0)
@@ -79,21 +130,17 @@ int parseFile(FILE *theFile)
 			sscanf(line, "%d", &trainingExamples);
 		}
 	}
-	int index = 0;
+	vector w; // todo where to put?
+	vector x;
+	trainingSession(theFile, dimension, trainingExamples, w);
 	while (fgets(line, 151, theFile) != NULL)
 	{
-		char *coordinate = NULL;
-		coordinate = strtok(line, ",");
-		puts("this is coordinate"); // todo remove
-		puts(coordinate); // todo remove
-//		strtod();
-		while ((coordinate = strtok(NULL, ",")) != NULL)
-		{
-			puts("this is coordinate"); // todo remove
-			puts(coordinate); // todo remove
-			// todo continue
-		}
+		x = lineToVector(line, dimension);
 	}
+//	for (int j = 0; j < MAX_VECTOR_LENGTH; ++j) // todo remove
+//	{
+//		printf("%f\n", x._coordinates[j]);
+//	}
 	return 0;
 }
 
--- LineSeparator.h
+++ LineSeparator.h
@@ -2,15 +2,38 @@
 
 #ifndef LINESEPARATOR_LINESEPARATOR_H
 #define LINESEPARATOR_LINESEPARATOR_H
-
+// ------------------------------ includes ------------------------------
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// -------------------------- const definitions -------------------------
+
 #define MAX_VECTOR_LENGTH 74
-typedef unsigned long array_length;
-// todo training function
+#define COORDINATE_SEPARATOR ","
 
-// todo separating function
+/**
+ * A struct that represents a vector, with maximum 74 coordinates.
+ */
+typedef struct vector {
+	int _label;
+	double _coordinates[MAX_VECTOR_LENGTH];
+} vector;
 
+// ------------------------------ functions -----------------------------
+
+/**
+ *
+ */
+int trainingSession(FILE*, int, int, vector);
+
+/**
+ *
+ */
+int separatingSession(FILE*);
+
+/**
+ *
+ */
+int sortData(FILE*); // todo maybe change to char*
 #endif //LINESEPARATOR_LINESEPARATOR_H
